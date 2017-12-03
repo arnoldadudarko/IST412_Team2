704ee918be1931215e197353d59e75fc0588abb3
Jimmy Hopf <jimer1995@yahoo.com>
2017-12-03 13:49:24 -0500

    Added PieChart to read the MockData.json file and present it

---
+++ Charts/PieChart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PieChart
+{
+    public void LoadJson()
+    {
+        using (StreamReader r = new StreamReader("MockData.json"))
+        {
+            string json = r.ReadToEnd();
+            List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+        }
+    }
+    dynamic array = JsonConvert.DeserializeObject(json);
+}
+

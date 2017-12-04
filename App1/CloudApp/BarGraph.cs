f2eb8c2bef28ecc5f6b3d0adb05e3b03f6537675
Jimmy Hopf <jimer1995@yahoo.com>
2017-12-03 19:37:56 -0500

    Began code for bar graph

---
+++ App1/CloudApp/BarGraph.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CloudApp
+{
+
+    public class BarGraph
+    {
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                List items = JsonConvert.DeserializeObject<List>(json);
+            }
+        }
+    }
+
+    internal class List
+    {
+    }
+}

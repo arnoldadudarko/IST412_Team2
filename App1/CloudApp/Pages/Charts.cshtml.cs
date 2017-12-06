91e97ec6b9d481aa2c2f7f6f65c7863b2a3b7f4f
flishman13 <flishman13@gmail.com>
2017-12-06 18:11:47 -0500

    * CloudApp.csproj: Added charts page
    * Charts.cshtml: Added charts page
    * _Layout.cshtml: Added charts page
    * Charts.cshtml.cs:  Added charts page

---
+++ App1/CloudApp/Pages/Charts.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudApp.Pages
+{
+    public class ChartsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

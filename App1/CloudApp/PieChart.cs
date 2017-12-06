99b82797c427f55689fd41bfe8c6c084d64f8c0f
ARNOLD ADU-DARKO <aka5317@psu.edu>
2017-12-06 18:09:34 -0500

    Pie Chart Test

---
+++ App1/CloudApp/PieChart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CloudApp
+{
+
+    public class PieChart
+    {
+        public PieChart()
+        {
+            string[] components = System.IO.File.ReadAllLines(@"C:\Users\Arnold Adu-Darko\Desktop\MockData.json");
+            System.Console.WriteLine("Components: ");
+            foreach (string line in components)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("Press a key to exit");
+            System.Console.ReadKey();
+        }   
+    }
+}
